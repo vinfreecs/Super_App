99c70a21d4ac404ca8c63351455bdf1d87ca589f
Vinay Patil <vinfreecs>
2023-12-09 21:28:34 +0530

    Removed timer

--- src/components/homepage/Timer.jsx
+++ src/components/homepage/Timer.jsx
@@ -1,64 +1,75 @@
-import { useState } from "react";
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
+// import { useState } from "react";
+// import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 export default function Timer() {
-    const [timer,setTimer] = useState({
-        status:false,
-        time:{
-            hours:0,
-            min:0,
-            sec:0,
-        }
-    })
-    const handleComplete = () =>{
-        setTimer({
-            status:false,
-            time:{
-                hours:null,
-                min:null,
-                sec:null,
-            }
-        })
-    }
-    const handleSubmitTimer =(e)=>{
-        e.preventDefault();
-        setTimer({...timer,status:true})
-    }
-    const handleChangeTime = (e) =>{
-        setTimer({status:false,time:{...timer.time,[e.target.id]:e.target.value}})
-    }
+    // const [timer,setTimer] = useState({
+    //     status:false,
+    //     time:{
+    //         hours:0,
+    //         min:0,
+    //         sec:0,
+    //     }
+    // })
+    // const handleComplete = () =>{
+    //     setTimer({
+    //         status:false,
+    //         time:{
+    //             hours:0,
+    //             min:0,
+    //             sec:0,
+    //         }
+    //     })
+    // }
+    // const handleSubmitTimer =(e)=>{
+    //     e.preventDefault();
+    //     console.log("invoked")
+    //     setTimer({...timer,status:true})
+    // }
+    // const handleChangeTime = (e) =>{
+    //     setTimer({status:false,time:{...timer.time,[e.target.id]:e.target.value}})
+    // }
+    // const toHoursMin=(time)=>{
+    //     const totalMin = Math.floor(time/60)
+    //     const seconds= time %60
+    //     const hours = Math.floor(totalMin/60)
+    //     const minutes = totalMin % 60
+    //     return `${hours}:${minutes}:${seconds}`
+    // }
   return (
     <>
     
       <div className="timer-wrapper">
         
-      <CountdownCircleTimer
+      {/* <CountdownCircleTimer
           isPlaying={timer.status}
-          duration={timer.time.sec}
+          duration={timer.time.sec + timer.time.min *60 + timer.time.hours*60*60}
           colors="#A30000"
-          onComplete={()=>{
-            handleComplete();
-            return {shouldRepeat:true,newInitialRemainingTime:timer.time.status}
-          }}
-        ></CountdownCircleTimer>
+          
+        >
+            {({remainingTime})=>{
+                <span style={{color:"white" , fontSize:"1.55rem"}}>
+                    {toHoursMin(remainingTime)}
+                </span>
+            }}
+        </CountdownCircleTimer>
         <form action="" onSubmit={handleSubmitTimer}>
             <div className="inputs-wrapper">
             <div className="hour-wrapper input-wrapper">
                 <span>Hours</span>
-            <input type="number" name="hours" id="hours" max={24} min={0} onChange={handleChangeTime}  />
+            <input type="number" name="hours" id="hours" max={24} min={0} value={timer.time.hours} onChange={handleChangeTime}  />
             </div>
             <div className="min-wrapper input-wrapper">
                 <span>Min</span>
-                <input type="number" name="min" id="min" max={60} min={0} onChange={handleChangeTime} />
+                <input type="number" name="min" id="min" max={60} min={0} value={timer.time.min} onChange={handleChangeTime} />
             </div>
             <div className="sec-wrapper input-wrapper">
                 <span>Sec</span>
-                <input type="number" name="sec" id="sec" max={60} min={0} onChange={handleChangeTime} value={timer.time.sec} />
+                <input type="number" name="sec" id="sec" max={60} min={0} value={timer.time.sec} onChange={handleChangeTime} />
             </div>
             </div>
             
             <button id="status">Start</button>
-        </form>
+        </form> */}
       </div>
     </>
   );
